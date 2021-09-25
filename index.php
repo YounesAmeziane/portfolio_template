1253fd682457c6f394c22baab40f648cd5fe998a
Younes Ameziane <73488317+YounesAmeziane@users.noreply.github.com>
2021-09-25 12:21:46 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,212 @@
+<!DOCTYPE html>
+<!-- Created By CodingNepal -->
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>L34x | Portfolio</title>
+    <link rel="stylesheet" href="s.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.11/typed.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"/>
+
+</head>
+<body>
+    <div class="scroll-up-btn">
+        <i class="fas fa-angle-up"></i>
+    </div>
+    <nav class="navbar">
+        <div class="max-width">
+            <div class="logo"><a href="#">L<span>34</span>X</a></div>
+            <ul class="menu">
+                <li><a href="#home" class="menu-btn">Home</a></li>
+                <li><a href="#about" class="menu-btn">À propose</a></li>
+                <li><a href="#services" class="menu-btn">Services</a></li>
+                <li><a href="#skills" class="menu-btn">Competences</a></li>
+                <li><a href="#contact" class="menu-btn">Contact</a></li>
+            </ul>
+            <div class="menu-btn">
+                <i class="fas fa-bars"></i>
+            </div>
+        </div>
+    </nav>
+
+    <!-- home section start -->
+    <section class="home" id="home">
+        <div class="max-width">
+            <div class="home-content">
+                <div class="text-1">Salut je m'appelle</div>
+                <div class="text-2">Younes Ameziane</div>
+                <div class="text-3">et je suis a <span class="typing"></span></div>
+                <a href="#">Engagez moi</a>
+            </div>
+        </div>
+    </section>
+
+    <!-- about section start -->
+    <section class="about" id="about">
+        <div class="max-width">
+            <h2 class="title">À propose</h2>
+            <div class="about-content">
+                <div class="column left">
+                    <img src="me.jpg" alt="">
+                </div>
+                <div class="column right">
+                    <div class="text">Je suis Younes et je suis a <span class="typing-2"></span></div>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt fuga praesentium laudantium, porro tempore temporibus omnis, ullam ipsum nihil natus eveniet dolorem modi eius unde repellat incidunt voluptatibus. Ad, tempora?Lorem, ipsum dolor sit amet </p>
+                    <a href="#">Télécharger CV</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- services section start -->
+    <section class="services" id="services">
+        <div class="max-width">
+            <h2 class="title">Mes Services</h2>
+            <div class="serv-content">
+                <div class="card">
+                    <div class="box">
+                        <i class="fas fa-code"></i>
+                        <div class="text">Devlopper</div>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt fuga praesentium laudantium, porro tempore</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <i class="fas fa-chart-line"></i>
+                        <div class="text">Advertising</div>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt fuga praesentium laudantium, porro tem</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- skills section start -->
+    <section class="skills" id="skills">
+        <div class="max-width">
+            <h2 class="title">Mes competences</h2>
+            <div class="skills-content">
+                <div class="column left">
+                    <div class="text">Mes creatives competences et experiences.</div>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt fuga praesentium laudantium, porro tempore temporibus omnis, ullam ipsum nihil natus eveniet dolorem modi eius unde repellat incidunt voluptatibus. Ad, tempora?Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt fuga praesentium laudantium, porro tempore temporibus omnis, ullam ipsum nihil natus eveniet dolorem modi eius unde repellat incidunt voluptatibus. Ad, tempora?</p>
+                    <a href="#">Read more</a>
+                </div>
+                <div class="column right">
+                    <div class="bars">
+                        <div class="info">
+                            <span>HTML</span>
+                            <span>90%</span>
+                        </div>
+                        <div class="line html"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>CSS</span>
+                            <span>70%</span>
+                        </div>
+                        <div class="line css"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>JavaScript</span>
+                            <span>30%</span>
+                        </div>
+                        <div class="line js"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>PHP</span>
+                            <span>65%</span>
+                        </div>
+                        <div class="line php"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>MySQL</span>
+                            <span>40%</span>
+                        </div>
+                        <div class="line mysql"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- contact section start -->
+    <section class="contact" id="contact">
+        <div class="max-width">
+            <h2 class="title">Contact moi</h2>
+            <div class="contact-content">
+                <div class="column left">
+                    <div class="text">Entrer en contac</div>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt fuga praesentium laudantium, porro tempore temporibus omnis</p>
+                    <div class="icons">
+                        <div class="row">
+                            <i class="fas fa-user"></i>
+                            <div class="info">
+                                <div class="head">Name</div>
+                                <div class="sub-title">I am somene</div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <i class="fas fa-map-marker-alt"></i>
+                            <div class="info">
+                                <div class="head">Address</div>
+                                <div class="sub-title">somewere</div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <i class="fas fa-envelope"></i>
+                            <div class="info">
+                                <div class="head">Email</div>
+                                <div class="sub-title">abc@gmail.com</div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <i class="fab fa-instagram"></i>
+                            <div class="info">
+                                <div class="head">Instagram</div>
+                                <div class="sub-title">username</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="column right">
+                    <div class="text">Contactez moi</div>
+                    <form action="#">
+                        <div class="fields">
+                            <div class="field name">
+                                <input type="text" placeholder="Name" required>
+                            </div>
+                            <div class="field email">
+                                <input type="email" placeholder="Email" required>
+                            </div>
+                        </div>
+                        <div class="field">
+                            <input type="text" placeholder="Subject" required>
+                        </div>
+                        <div class="field textarea">
+                            <textarea cols="30" rows="10" placeholder="Message.." required></textarea>
+                        </div>
+                        <div class="button-area">
+                            <button type="submit">Envoyer</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- footer section start -->
+    <footer>
+        <span>Created By <a href="https://www.l34x.ml">Leax</a> | <span class="far fa-copyright"></span> 2021 All rights reserved.</span>
+    </footer>
+
+    <script src="jsnav.js"></script>
+</body>
+</html>
